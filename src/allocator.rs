e7629d5af3731a33a1cc4899300c086f0dddc665
Emilia Daelman <emilia.daelman@gmail.com>
2025-02-16 11:44:55 +0100

    implement fixed block allocator

--- src/allocator.rs
+++ src/allocator.rs
@@ -1,16 +1,17 @@
 pub mod bump;
+pub mod fixed_size_block;
 pub mod linked_list;
 
-use linked_list::LinkedListAllocator;
+use fixed_size_block::FixedBlockSizeAllocator;
 use x86_64::{
     structures::paging::{
-        mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB
+        mapper::MapToError, FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB,
     },
     VirtAddr,
 };
 
 #[global_allocator]
-static ALLOCATOR: Locked<LinkedListAllocator> = Locked::new(LinkedListAllocator::new());
+static ALLOCATOR: Locked<FixedBlockSizeAllocator> = Locked::new(FixedBlockSizeAllocator::new());
 
 pub const HEAP_START: usize = 0x_4444_4444_0000;
 pub const HEAP_SIZE: usize = 100 * 1024;
@@ -48,9 +49,7 @@ pub fn init_heap(
             .allocate_frame()
             .ok_or(MapToError::FrameAllocationFailed)?;
         let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
-        unsafe {
-            mapper.map_to(page, frame, flags, frame_allocator)?.flush()
-        };
+        unsafe { mapper.map_to(page, frame, flags, frame_allocator)?.flush() };
     }
 
     unsafe {
